7bb8b69133d10d13766774f6154475099d8fc2c5
P1ZZ4 <30331389+P1ZZ4@users.noreply.github.com>
2019-11-16 02:11:20 +0900

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,12 @@
 class Post {
   static create(row) {
-    const post = new Post(row.id, row.sid, row.title, row.content, row.hidden, row.views, row.likes, row.created_at, row.board_id);
+    const post = new Post(row.id, row.sid, row.title,
+      row.content, row.hidden, row.views, row.likes,
+      row.created_at, row.board_id);
     return post;
   }
 
-  constructor(id, sid, title, content, hidden, views, likes, created_at, board_id) {
+  constructor(id, sid, title, content, hidden, views, likes, createdAt, boardId) {
     this.id = id;
     this.sid = sid;
     this.title = title;
@@ -12,13 +14,13 @@ class Post {
     this.hidden = hidden;
     this.views = views;
     this.likes = likes;
-    this.created_at = created_at;
-    this.board_id = board_id;
+    this.createdAt = createdAt;
+    this.boardId = boardId;
   }
 
   toJSON() {
     const {
-      id, sid, title, content, hidden, views, likes, created_at, board_id,
+      id, sid, title, content, hidden, views, likes, createdAt, boardId,
     } = this;
     return {
       id,
@@ -28,8 +30,8 @@ class Post {
       hidden,
       views,
       likes,
-      created_at,
-      board_id,
+      createdAt,
+      boardId,
     };
   }
 }
