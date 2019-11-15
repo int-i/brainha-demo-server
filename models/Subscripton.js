525ce335c7c4dd0b2878dcf6cd542be2632a5505
P1ZZ4 <30331389+P1ZZ4@users.noreply.github.com>
2019-11-16 02:11:39 +0900

    Update Subscripton.js

--- models/Subscripton.js
+++ models/Subscripton.js
@@ -1,31 +1,33 @@
 class Subscription {
   static create(row) {
-    const subscription = new subscription(row.id, row.plan_id, row.user_id, row.valid, row.started_at, row.ended_at, row.canceled_at);
+    const subscription = new Subscription(row.id, row.plan_id,
+      row.user_id, row.valid, row.started_at, row.ended_at,
+      row.canceled_at);
     return subscription;
   }
 
-  constructor(id, plan_id, user_id, valid, started_at, ended_at, canceled_at) {
+  constructor(id, planId, userId, valid, startedAt, endedAt, canceledAt) {
     this.id = id;
-    this.plan_id = plan_id;
-    this.user_id = user_id;
+    this.planId = planId;
+    this.userId = userId;
     this.valid = valid;
-    this.started_at = started_at;
-    this.ended_at = ended_at;
-    this.canceled_at = canceled_at;
+    this.startedAt = startedAt;
+    this.endedAt = endedAt;
+    this.canceledAt = canceledAt;
   }
 
   toJSON() {
     const {
-      id, plan_id, user_id, valid, started_at, ended_at, canceled_at,
+      id, planId, userId, valid, startedAt, endedAt, canceledAt,
     } = this;
     return {
       id,
-      plan_id,
-      user_id,
+      planId,
+      userId,
       valid,
-      started_at,
-      ended_at,
-      canceled_at,
+      startedAt,
+      endedAt,
+      canceledAt,
     };
   }
 }
