9cd2eff7a59aa6efd7f2b3000166907c4ab0a8ef
P1ZZ4 <30331389+P1ZZ4@users.noreply.github.com>
2019-11-16 02:10:38 +0900

    Update and rename Comment_box.js to CommentBox.js

---
+++ models/CommentBox.js
@@ -0,0 +1,34 @@
+class CommentBox {
+  static create(row) {
+    const commentBox = new CommentBox(row.id, row.sid, row.content,
+      row.hidden, row.likes, row.created_at, row.post_id);
+    return commentBox;
+  }
+
+  constructor(id, sid, content, hidden, likes, createdAt, postId) {
+    this.id = id;
+    this.sid = sid;
+    this.content = content;
+    this.hidden = hidden;
+    this.likes = likes;
+    this.createAt = createdAt;
+    this.postId = postId;
+  }
+
+  toJSON() {
+    const {
+      id, sid, content, hidden, likes, createdAt, postId,
+    } = this;
+    return {
+      id,
+      sid,
+      content,
+      hidden,
+      likes,
+      createdAt,
+      postId,
+    };
+  }
+}
+
+module.exports = CommentBox;
