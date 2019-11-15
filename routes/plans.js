546bad420db198d57a0b72fe7035a1edfbf1720f
P1ZZ4 <30331389+P1ZZ4@users.noreply.github.com>
2019-11-16 01:26:39 +0900

    Create plans.js

---
+++ routes/plans.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+const plans = require('../controllers/plans');
+
+const router = Router();
+
+router.get('/', plans.getPlans);
+router.get('/:id', plans.getPlan);
+
+module.exports = router;
