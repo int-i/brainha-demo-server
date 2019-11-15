efab7d8a9d05ae25cb921e296ccbddc551dad474
P1ZZ4 <30331389+P1ZZ4@users.noreply.github.com>
2019-11-16 02:11:03 +0900

    Update Plan.js

--- models/Plan.js
+++ models/Plan.js
@@ -1,26 +1,27 @@
 class Plan {
-    static create(row) {
-      const plan = new Plan(row.id, row.name, row.cost, row.duration);
-      return plan;
-    }
-  
-    constructor(id, name, cost, duration) {
-      this.id = id;
-      this.name = name;
-      this.cost = cost;
-      this.duration = duration;
-    }
-  
-    toJSON() {
-      const { id, name, cost, duration } = this;
-      return {
-        id,
-        name,
-        cost,
-        duration
-      };
-    }
+  static create(row) {
+    const plan = new Plan(row.id, row.name, row.cost, row.duration);
+    return plan;
   }
-  
-  module.exports = Plan;
-  
+
+  constructor(id, name, cost, duration) {
+    this.id = id;
+    this.name = name;
+    this.cost = cost;
+    this.duration = duration;
+  }
+
+  toJSON() {
+    const {
+      id, name, cost, duration,
+    } = this;
+    return {
+      id,
+      name,
+      cost,
+      duration,
+    };
+  }
+}
+
+module.exports = Plan;
